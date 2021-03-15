504db20724358186ab60bdb92f8716fe29fc1651
Toooble <tobiclement83@gmail.com>
2021-03-15 11:14:36 +0000

    Small changes and refactors
    Removed Clock and Array Script.
    Combined both into GameHandler Script
    
    Removed the need to manually add flowers to the array
    
    Created separate functions for handling array and the clock.
    
    Changed UI variables in preparation for larger UI changes
    
    Readability changes

---
+++ Group 16 Gardening Game/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+//This Script Handles the game loop
+
+public class GameHandler : MonoBehaviour
+{
+
+    //Flower Vars
+    
+    private GameObject[] Flowers; //Created GameObject Array Flowers 
+    private int NumberOfFlowers; // Number of flowers in total
+    private int NumberDead = 0; // Records number of dead flowers
+
+    //Clock Vars
+    public float totalTime; // the timer length
+    private float minutes;//as it says
+    private float seconds;
+
+    //UI Vars 
+    public Text FlowerText;
+    public Text ClockText;
+
+    void Start()
+    {
+        FindFlowers();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Clock();
+        FlowerCheck();
+
+    }
+
+
+    void FindFlowers()
+    {
+        //Finds all flowers
+        Flowers = GameObject.FindGameObjectsWithTag("Flower");
+
+        //For each flower in the array
+        foreach (GameObject o in Flowers)
+        {
+            // Add to number of flowers
+            NumberOfFlowers++;
+
+            Debug.Log("Flower noticed" + NumberOfFlowers.ToString());  //debug
+        }
+    }
+
+    void FlowerCheck()
+    {
+        NumberDead = 0; // resets the variable so we have a live reading at all times
+        foreach (var Dead in Flowers)// for every flower in the array
+        {
+            if (Dead.GetComponent<FlowerScript>().IsDead)// get the IsDead variable from the current flower
+            {
+                Debug.Log(NumberDead.ToString());// debug
+                NumberDead++;//increase number of dead
+
+            }
+        }
+        FlowerText.text = (NumberOfFlowers - NumberDead).ToString() + "/" + NumberOfFlowers.ToString();
+    }
+
+    void Clock()
+    {
+        totalTime -= Time.deltaTime; // reduce timer
+
+        minutes = (int)(totalTime / 60);
+        seconds = (int)(totalTime % 60);
+
+        ClockText.text = minutes.ToString() + " : " + seconds.ToString();
+    }
+
+    //TO DO
+    /*
+     * Format out Number of flowers minus the number of dead out of number of flowers, so (3/6)
+     * 
+     * Display formated text out to a text object
+     * 
+     * code cleaning
+     * 
+     * Array Creates flowers
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+}
