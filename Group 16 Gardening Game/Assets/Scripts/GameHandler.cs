e8db8c6a858ac7546687745bd60dc83aa22b296e
Sam Myatt <smyatt@rocketmail.com>
2021-03-19 17:27:07 +0000

    Flower Health Fixed Menu Started
    The begingins work for the menu's and the health for plants is fixed

---
+++ Group 16 Gardening Game/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,169 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//This Script Handles the game loop
+
+public class GameHandler : MonoBehaviour
+{
+
+    //Flower Vars
+    private GameObject[] Flowers; //Created GameObject Array Flowers 
+    private int NumberOfFlowers; // Number of flowers in total
+    private int NumberDead = 0; // Records number of dead flowers
+    public int FlowersRequired;
+
+    //Clock Vars
+    public float totalTime; // the timer length
+    private float minutes;//as it says
+    private float seconds;
+
+    //UI Vars 
+    public Text FlowerText;
+    public Text ClockText;
+    public Text Score;
+
+    //Level Loader Vars
+    public int CurrentLevel;
+    private int NextLevel;
+    public static bool GameIsOver = false;
+    public GameObject EndMenuUI;
+
+    void Start()
+    {
+        FindFlowers();
+        DontDestroyOnLoad(gameObject);
+        NextLevel = CurrentLevel + 1;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Clock();
+        FlowerCheck();
+       // ClockCheck();
+    }
+
+
+    void FindFlowers()
+    {
+        //Finds all flowers
+        Flowers = GameObject.FindGameObjectsWithTag("Flower");
+
+        //For each flower in the array
+        foreach (GameObject o in Flowers)
+        {
+            // Add to number of flowers
+            NumberOfFlowers++;
+
+            Debug.Log("Flower noticed" + NumberOfFlowers.ToString());  //debug
+        }
+    }
+
+    void FlowerCheck()
+    {
+        NumberDead = 0; // resets the variable so we have a live reading at all times
+        foreach (var Dead in Flowers)// for every flower in the array
+        {
+            if (Dead.GetComponent<FlowerScript>().IsDead)// get the IsDead variable from the current flower
+            {
+                //Debug.Log(NumberDead.ToString());// debug
+                NumberDead++;//increase number of dead
+
+            }
+        }
+        FlowerText.text = (NumberOfFlowers - NumberDead).ToString() + "/" + NumberOfFlowers.ToString();
+    }
+
+    void Clock()
+    {
+        if (totalTime >= 0)
+        {
+            totalTime -= Time.deltaTime; // reduce timer
+        }
+        else
+        {
+            StartCoroutine(ClockCheck());
+        }
+
+        minutes = (int)(totalTime / 60);
+        seconds = (int)(totalTime % 60);
+
+        ClockText.text = minutes.ToString() + " : " + seconds.ToString();
+
+    }
+
+
+    IEnumerator ClockCheck()
+    {
+
+        if ((NumberOfFlowers - NumberDead) >= FlowersRequired)
+        {
+            //Win the level
+            GameIsOver = true;
+            EndMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            //LoadScene();
+        }
+        else
+        {
+            GameIsOver = true;
+            EndMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+        }
+            //Lose the level   
+        yield return null; 
+    }
+
+    //ORIGINAL CLOCK CHECK
+
+    /*void ClockCheck()
+    {
+        if(totalTime <= 0)
+        {
+            //Timer is up
+            if((NumberOfFlowers - NumberDead) >= FlowersRequired)
+            {
+                //Win the level
+                LoadScene();
+            }
+            else
+            {
+
+            }
+            //Lose the level   
+        }
+    }
+    */
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(NextLevel);
+        CurrentLevel = NextLevel;
+        NextLevel++;
+    }
+
+    //TO DO
+    /*
+     * Format out Number of flowers minus the number of dead out of number of flowers, so (3/6)
+     * 
+     * Dis.y formated text out to a text object
+     * 
+     * code cleaning
+     * 
+     * Array Creates flowers
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+}
---
+++ Group 16 Gardening Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,197 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour
+{
+    //Player rigid body
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    //Player vector used for movement
+    Vector2 movement;
+    Vector2 moveDir;
+    Vector2 prevDir;
+
+
+    //Player Game Vars
+    public float moveSpeed = 5.0f;
+    public string statusEffect;
+    public Vector2 plantPos;
+
+    //Flag for flower range
+    bool inFlowerRange;
+    //Plant healthbar
+    public Slider plantHealth;
+
+    public bool hasCan = false;
+    public bool hasFertilizer = false;
+    GameObject WateringCan;
+    GameObject Fertalizer;
+
+
+    //Stores Current Flower
+    GameObject CurrFlower;
+
+    private void Start()
+    {
+        WateringCan = GameObject.FindWithTag("WaterCan");
+        Fertalizer = GameObject.FindWithTag("Fertalizer");
+    }
+    void Update()
+    {
+        //StartCoroutine("Movement");
+        Movement();
+        Animate();
+        DropCan();
+
+    }
+
+    void FixedUpdate()
+    {
+        // MOVE PLAYERS
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        //Updates PlantHealth Position independant of functions
+        //(Mostly stops glitchy behaviour)
+
+        //THIS IS WHY THE 
+        //plantHealth.transform.position = playerPos;
+
+        if (inFlowerRange == true)
+        {
+            PlantHealth();
+        }
+    }
+
+    void Movement()
+    {
+        //GET INPUTS
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        moveDir = new Vector2(movement.x, movement.y).normalized;
+
+        if ((movement.x == 0 && movement.y == 0) && moveDir.x != 0 || moveDir.y != 0)
+        {
+            prevDir.x = moveDir.x;
+            prevDir.y = moveDir.y;
+
+        }
+
+        if (Input.GetKeyDown("space") && (inFlowerRange == true))
+        {
+            //Log to see if input is recognised
+            Debug.Log("Rejuvenating...");
+
+            //Targets the collided with flower and runs Rejuvinate
+
+            // Detects what kind of flower the player has ran into
+            if (CurrFlower.tag == "Flower"&& hasCan)
+            {
+                CurrFlower.GetComponent<FlowerScript>().Rejuvenate();
+            }
+            if (CurrFlower.tag == "FertFlower" && hasFertilizer)
+            {
+                CurrFlower.GetComponent<FlowerScript>().Rejuvenate();
+            }
+        }
+
+    }
+    void DropCan()
+    {
+        // drops the current item you have equiped
+        if (WateringCan== true && Input.GetKey(KeyCode.Q))
+        {
+            hasCan = false;
+            WateringCan.SetActive(true);
+            WateringCan.transform.parent = null;
+
+        }
+        if (Fertalizer == true && Input.GetKey(KeyCode.Q))
+        {
+            hasFertilizer = false;
+            Fertalizer.SetActive(true);
+            Fertalizer.transform.parent = null;
+
+        }
+    }
+
+    void PlantHealth()
+    {
+        //Grabs current flower's hp each frame
+        6
+        //Add Y offset if possible
+    }
+
+
+    //Detects when player enters/exits a flower's range.
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        //Checks if the player is in the Flower's range
+        inFlowerRange = true;
+        //Debug.Log("In Range");
+
+        Debug.Log("In Range");
+        //Assigns the collided with flower to the Current Flower var
+        CurrFlower = collider.gameObject;
+
+        //Displays current flower
+        Debug.Log(CurrFlower);
+
+        //Enable Plant Healthbar
+        plantHealth.gameObject.SetActive(true);
+
+        plantPos = CurrFlower.transform.position;
+        plantHealth.transform.position = plantPos;
+
+    }
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        // player can equip watering can
+        if (collider.gameObject.tag == "WaterCan" && !hasCan)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                Debug.Log("Pickup");
+                WateringCan.transform.parent = gameObject.transform;
+                WateringCan.SetActive(false);
+                hasCan = true;
+
+            }
+
+        }
+        // player can equip fertalizer
+        if (collider.gameObject.tag == "Fertalizer" && !hasFertilizer)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                Debug.Log("Pickup");
+                Fertalizer.transform.parent = gameObject.transform;
+                Fertalizer.SetActive(false);
+                hasFertilizer = true;
+
+            }
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        //Disable health bar
+        plantHealth.gameObject.SetActive(false);
+
+        Debug.Log("Left Range");
+
+        //Resets the var for when the player leaves the flower's range
+        inFlowerRange = false;
+    }
+
+    void Animate()
+    {
+        animator.SetFloat("AnimMoveX", movement.x);
+        animator.SetFloat("AnimMoveY", movement.y);
+        animator.SetFloat("AnimMoveForce", movement.magnitude);
+        animator.SetFloat("PrevMoveX", prevDir.x);
+        animator.SetFloat("PrevMoveY", prevDir.y);
+    }
+}
