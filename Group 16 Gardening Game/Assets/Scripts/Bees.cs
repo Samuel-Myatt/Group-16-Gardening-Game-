cf0c0f8ee35ec5dc8a44e030b358f82501513d54
Torterrad <64802272+Torterrad@users.noreply.github.com>
2021-03-20 19:22:48 +0000

    bees and art assets
    added bees and medal animations along with some art assets. bees move around to certain points randomly but don't stun the player yet. should be easy once the status system is done

---
+++ Group 16 Gardening Game/Assets/Scripts/Bees.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bees : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] moveSpots;//array of spots bees can move between
+    private int randomSpot;
+
+    void Start()
+    {
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
+
+    }
+
+    void Update()
+    {   //moves bees position to random spot in the array
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        
+
+        //if the bees are with 0.2 of their target location
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            
+            if(waitTime <= 0) 
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);//new random location
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+                
+        }
+    }
+}
